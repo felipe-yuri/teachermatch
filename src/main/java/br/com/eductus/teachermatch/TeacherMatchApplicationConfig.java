98dfa2188e67335b546068be3d00a70e6d191651
Felipe Yuri Silva <felipe.yuri.silva@everis.com>
2020-07-01 19:54:16 -0300

    War e tomcat

--- src/main/java/br/com/eductus/teachermatch/TeacherMatchApplicationConfig.java
+++ src/main/java/br/com/eductus/teachermatch/TeacherMatchApplicationConfig.java
@@ -2,9 +2,10 @@ package br.com.eductus.teachermatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TeacherMatchApplicationConfig {
+public class TeacherMatchApplicationConfig extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(TeacherMatchApplicationConfig.class, args);
