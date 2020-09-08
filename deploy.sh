60b216aa193faee8f99a613a8bbbc1ffd2ec49c2
Felipe Yuri Silva <felipys@gmail.com>
2020-09-08 00:15:03 -0300

    Melhorando deploy com \n

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-echo "\n"
+echo -e "\n"
 echo "Iniciando o deploy automático"
-echo "\n"
+echo -e "\n"
 
 echo "1. Atualização da branch (pull)"
-echo "\n"
+echo -e "\n"
 git pull
-echo "\n"
+echo -e "\n"
 
 echo "2. Limpando e gerando package"
-echo "\n"
+echo -e "\n"
 mvn clean package
-echo "\n"
+echo -e "\n"
 
 echo "3. Movendo package.war para a pasta de deploy do tomcat"
-echo "\n"
+echo -e "\n"
 mv target/teachermatch-0.0.1-SNAPSHOT.war /home/felipys/apps/apache-tomcat-8.5.56/webapps/ROOT.war
-echo "\n"
+echo -e "\n"
 
 echo "4. Deploy finalizado com sucesso!!"
-echo "\n"
+echo -e "\n"
