0fe6531619e831e8ff763b64e5f01809567bb965
Felipe Yuri Silva <felipys@gmail.com>
2020-09-09 15:15:37 -0300

    correção email e descrição dos processos

--- src/main/java/br/com/eductus/teachermatch/services/EmailService.java
+++ src/main/java/br/com/eductus/teachermatch/services/EmailService.java
@@ -27,31 +27,32 @@ public class EmailService {
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
 		String corpoEmail = "<!DOCTYPE html>" +
-		"<html>" +
-		"<head>" +
-		"  <title>Mentoriza</title>" +
-		"	<meta charset=\"UTF-8\"/>" +
-		"	<meta name=\"viewport\" content=\"width = device-width, initial-scale = 1\" />" +
-		"</head>" +
-		"<body class=\"container\">" +
-		"" +
-		"	<div class=\"card-panel\" align=\"center\" style=\"background-color: #0c80ac;\">" +
-		"		<h3 style=\"font-size:4vw;\"><font face=\"Holtwood One SC, serif\" color=\"white\">MENTORIZA</font></h3>" +
-		"	</div>" +
-		"	<br>" +
-		"	<div class=\"row\">" +
-		"		<div>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Olá <b>" + nome +"</b>,</p>" + 
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">O seu cadastro foi finalizado com sucesso. <b>Parabéns!</b></p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Fique atento para novas informações. Bons estudos e até breve!</p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Atenciosamente,</p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Equipe Eductus</p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Teste <b>"+ mentores.getPontuacao() +" pontos</b>.</p>" +
-		"		</div>" +
-		"		<br>" +
-		"	</div>" +
-		"</body>" +
-		"</html>";
+				"<html>" +
+				"<head>" +
+				"  <title>Mentoriza</title>" +
+				"	<meta charset=\"UTF-8\"/>" +
+				"	<meta name=\"viewport\" content=\"width = device-width, initial-scale = 1\" />" +
+				"</head>" +
+				"<body class=\"container\">" +
+				"	" +
+				"	<div class=\"card-panel\" align=\"center\" style=\"background-color: #0c80ac;\">" +
+				"		<h3 style=\"font-size:4vw;\"><font face=\"Holtwood One SC, serif\" color=\"white\">MENTORIZA</font></h3>" +
+				"	</div>" +
+				"	<br>" +
+				"	<div class=\"row\">" +
+				"		<div>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Olá <b>" + nome + "</b>,</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">O seu cadastro para o projeto Mentoriza foi finalizado com sucesso.</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Estamos buscando impactar a educação no Brasil e ficamos muito felizes com o seu interesse em participar!</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Nós entraremos em contato em breve. Fique ligado nas nossas redes sociais para mais informações.</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Obrigado!</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Atenciosamente,</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Equipe Mentoriza</p>" +
+				"		</div>" +
+				"		<br>" +
+				"	</div>" +
+				"</body>" +
+				"</html>";
 		
 		helper.setText(corpoEmail, true); 
 		helper.setTo(para);
@@ -66,31 +67,32 @@ public class EmailService {
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
 		String corpoEmail = "<!DOCTYPE html>" +
-		"<html>" +
-		"<head>" +
-		"  <title>Mentoriza</title>" +
-		"	<meta charset=\"UTF-8\"/>" +
-		"	<meta name=\"viewport\" content=\"width = device-width, initial-scale = 1\" />" +
-		"</head>" +
-		"<body class=\"container\">" +
-		"" +
-		"	<div class=\"card-panel\" align=\"center\" style=\"background-color: #0c80ac;\">" +
-		"		<h3 style=\"font-size:4vw;\"><font face=\"Holtwood One SC, serif\" color=\"white\">MENTORIZA</font></h3>" +
-		"	</div>" +
-		"	<br>" +
-		"	<div class=\"row\">" +
-		"		<div>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Olá <b>" + nome +"</b>,</p>" + 
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">O seu cadastro foi finalizado com sucesso. <b>Parabéns!</b></p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Fique atento para novas informações. Bons estudos e até breve!</p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Atenciosamente,</p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Equipe Eductus</p>" +
-		"			<p style=\"text-align: justify; text-justify: inter-word;\">Teste <b>"+ mentorandos.getPontuacao() +" pontos</b>.</p>" +
-		"		</div>" +
-		"		<br>" +
-		"	</div>" +
-		"</body>" +
-		"</html>";
+				"<html>" +
+				"<head>" +
+				"  <title>Mentoriza</title>" +
+				"	<meta charset=\"UTF-8\"/>" +
+				"	<meta name=\"viewport\" content=\"width = device-width, initial-scale = 1\" />" +
+				"</head>" +
+				"<body class=\"container\">" +
+				"	" +
+				"	<div class=\"card-panel\" align=\"center\" style=\"background-color: #0c80ac;\">" +
+				"		<h3 style=\"font-size:4vw;\"><font face=\"Holtwood One SC, serif\" color=\"white\">MENTORIZA</font></h3>" +
+				"	</div>" +
+				"	<br>" +
+				"	<div class=\"row\">" +
+				"		<div>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Olá <b>" +nome + "</b>,</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">O seu cadastro para o projeto Mentoriza foi finalizado com sucesso.</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Estamos buscando impactar a educação no Brasil e ficamos muito felizes com o seu interesse em participar!</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Nós entraremos em contato em breve. Fique ligado nas nossas redes sociais para mais informações.</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Obrigado!</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Atenciosamente,</p>" +
+				"			<p style=\"text-align: justify; text-justify: inter-word;\">Equipe Mentoriza</p>" +
+				"		</div>" +
+				"		<br>" +
+				"	</div>" +
+				"</body>" +
+				"</html>";
 		
 		helper.setText(corpoEmail, true); 
 		helper.setTo(para);
