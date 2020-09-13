953efbb278d11bfe975c9e63f967a14c99c4e7d5
root <root@mentoriza.southamerica-east1-b.c.eductus-71812.internal>
2020-09-13 18:20:50 +0000

    correção deploy para novo servidor

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ echo -e "\n"
 
 echo "3. Movendo package.war para a pasta de deploy do tomcat"
 echo -e "\n"
-mv target/teachermatch-0.0.1-SNAPSHOT.war /home/felipys/apps/apache-tomcat-8.5.56/webapps/ROOT.war
+mv target/teachermatch-0.0.1-SNAPSHOT.war ~/apps/apache-tomcat-8.5.57/webapps/ROOT.war
 echo -e "\n"
 
 echo "4. Deploy finalizado com sucesso!!"
