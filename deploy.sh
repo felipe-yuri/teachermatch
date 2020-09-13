0233ae2812dd96b7c453b74431321ba89674d4d4
Felipe Yuri Silva <felipys@gmail.com>
2020-09-13 17:49:17 -0300

    add sudo em deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@ echo -e "\n"
 
 echo "1. Atualização da branch (pull)"
 echo -e "\n"
-git pull
+sudo git pull
 echo -e "\n"
 
 echo "2. Limpando e gerando o package"
 echo -e "\n"
-mvn clean package
+sudo mvn clean package
 echo -e "\n"
 
 echo "3. Movendo package.war para a pasta de deploy do tomcat"
 echo -e "\n"
-mv target/teachermatch-0.0.1-SNAPSHOT.war ~/apps/apache-tomcat-8.5.57/webapps/ROOT.war
+sudo mv target/teachermatch-0.0.1-SNAPSHOT.war ~/apps/apache-tomcat-8.5.57/webapps/ROOT.war
 echo -e "\n"
 
 echo "4. Deploy finalizado com sucesso!!"
